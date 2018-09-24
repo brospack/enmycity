36aad4c39a0f3706e02333a105b270310a9e1fd7
Alejandro Checa <alejandro.checa@scmspain.com>
2018-09-24 13:24:36 +0200

    resolve conflict

--- app/src/main/java/com/android/enmycity/search/SearchFragment.kt
+++ app/src/main/java/com/android/enmycity/search/SearchFragment.kt
@@ -1,6 +1,5 @@
 package com.android.enmycity.search
 
-import android.app.Fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
